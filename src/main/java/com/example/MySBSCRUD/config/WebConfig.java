511747632fd0b903e1d2a0d8ed6ffbdd1b962d72
Amru05 <77205579+Amru05@users.noreply.github.com>
2021-06-29 22:00:25 +0300

    Add files via upload

---
+++ src/main/java/com/example/MySBSCRUD/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.MySBSCRUD.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// класс для предоставления конфигурации MVC
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/news").setViewName("news");
+
+    }
+}
