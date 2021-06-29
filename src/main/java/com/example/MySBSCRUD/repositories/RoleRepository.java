9fb21296c352e8bf2c944a0f1e6c84e2b0cdcead
Amru05 <77205579+Amru05@users.noreply.github.com>
2021-06-29 21:57:06 +0300

    Delete src/main/java/com/example/MySBSCRUD directory

--- src/main/java/com/example/MySBSCRUD/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MySBSCRUD.repositories;
-
-import com.example.MySBSCRUD.entities.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
